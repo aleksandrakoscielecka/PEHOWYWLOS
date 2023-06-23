f24cb366e23349616fcda121b12f41deaff7101c
aleksandrakoscielecka <137445783+aleksandrakoscielecka@users.noreply.github.com>
2023-06-23 10:23:53 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'accent': '#cfb99d',
+      },
+    },
+  },
+  plugins: [],
+}
